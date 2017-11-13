a0e276e4921572dffc188c881d697af4641cfd1a
Yuri07 <yuri_resende@hotmail.com>
2017-11-13 16:22:14 -0300

    Exercicio -Imagem circular carregada pela biblioteca glide- resolvido

---
+++ app/src/main/java/edu/galileo/mvp/edxGlideImgApp.java
@@ -0,0 +1,8 @@
+package edu.galileo.mvp;
+
+/**
+ * Created by yuri_ on 13/11/2017.
+ */
+
+public class edxGlideImgApp {
+}
---
+++ app/src/main/java/edu/galileo/mvp/lib/GlideImageLoader.java
@@ -0,0 +1,8 @@
+package edu.galileo.mvp.lib;
+
+/**
+ * Created by yuri_ on 13/11/2017.
+ */
+
+public class GlideImageLoader {
+}
